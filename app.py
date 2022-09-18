1fc024a9b54085bfe5f4b5db2b6d71401253c9cc
martindamian13 <cachobnitz@icloud.com>
2022-09-18 15:07:30 -0400

    Inicio de html y debug mode activado

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, g
+import sqlite3
 
 app = Flask(__name__)
+# Database
 
+# Ruta principal
 @app.route('/')
-def hello_world():
+def index():
+    return render_template('index.html')
 
-    nombre = "martin"
-    apellido = "quintana"
-    return 'Hello, World!'
-
+# Debug mode activado
+if __name__ == '__main__':
+    app.run(debug=True)
