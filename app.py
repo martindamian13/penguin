ac2fc4d7b4899ba27b901f850241d2db34415c5f
mategri <mariatgrino.97@gmail.com>
2022-09-18 18:00:07 +0200

    Probando cosas

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-    # esto escribio mate
+    # esto escribio mate 
 
-nombre = mate 
+nombre = 'mate' 
 # otro comentario nuevo 
 
+
+if __name__ == '__main__':
+    app.run(debug=True)
