3c50936e1852053a2d23f5da4e72b093cb9634ad
mategri <mariatgrino.97@gmail.com>
2022-09-20 00:05:34 +0200

    id cambiados a variables

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def get_db():
     return g.sqlite_db
 
 @app.teardown_appcontext
-def close_db(error):
+def close_db(error): 
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
@@ -24,7 +24,7 @@ def close_db(error):
 @app.route('/')
 def hello_world():
     db = get_db()
-    cur = db.execute('SELECT * FROM ejemplo')
+    cur = db.execute('SELECT * FROM database')
     datas = cur.fetchall()
     return render_template('index.html', datas=datas)    
 
@@ -36,11 +36,21 @@ def form():
 def agregar():   
     if request.method == 'POST':
         try:
-            nombre = request.form['nombre']
-            edad = request.form['edad']
+            nombre = request.formartesanos['nombre']
+            apellido = request.formartesanos['apellido']
+            cuidad = request.formartesanos['cuidad']
+            nacimiento = request.formartesanos['nacimiento']
+            sexo = request.formartesanos['sexo']
+            t_artesanal = request.formartesanos['t_artesanal']
+            modalidad = request.formartesanos['modalidad']
+            mat_prima = request.formartesanos['mat_prima']
+            oficio = request.formartesanos['oficio']
+            antiguedad =request.formartesanos['antiguedad']
+
+
             with sqlite3.connect("database.db") as con:
                 cur = con.cursor()
-                cur.execute("INSERT INTO ejemplo (nombre, edad) VALUES (?,?)",(nombre,edad) )
+                cur.execute("INSERT INTO ejemplo (nombre, apellido, cuidad, nacimiento, sexo, t_artesanal, modalidad, mat_prima, oficio, antiguedad ) VALUES (?,?,?,?,?,?,?,?,?,?)",(nombre, apellido, cuidad, nacimiento, sexo, t_artesanal, modalidad, mat_prima, oficio, antiguedad) )
                 con.commit()
                 msg = "Record successfully added"
         except:
