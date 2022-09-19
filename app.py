2b03dc7751ffbc2e0e5909837f4bb377ac691f2c
DEMONDHUNTER <ricarf.ferreira@gmail.com>
2022-09-19 09:35:26 -0400

    datos del tutorial de sqlite

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask
+from http.client import REQUEST_ENTITY_TOO_LARGE
+from wsgiref.handlers import read_environ
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/tasks.db'
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(250))
+    done = db.Column(db.Boolean)
 
 @app.route('/')
-def hello_world(): # what
-    return 'Hello, World!' #ag
+def home():
+    return render_template('index.html')
+
+
+@app.route('/create-registro', methods=['POST'])
+def create():
+    task = Task(content=request.form['content'], done=False)
+    db.session.add(task)
+    db.session.commit()
+    return 'saved'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
